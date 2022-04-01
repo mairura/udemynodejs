5a8387217a6d171405db3612ecb17dc18f632faf
unknown <mairurated@gmail.com>
2022-04-01 15:31:59 +0300

    Some practise on Nodejs and MongoDB

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port: ${port}...`);
