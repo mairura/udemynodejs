073209f811483d838f720c4677516f64ca53718d
unknown <mairurated@gmail.com>
2022-04-04 15:55:21 +0300

    connection to mongoose

--- server.js
+++ server.js
@@ -1,7 +1,57 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// console.log(process.env);
+
+// mongoose.connect(
+//   'mongodb+srv://ted:Teddybryn146704449@cluster0.7l72b.mongodb.net/natours?retryWrites=true'
+// );
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('Database connection was successful'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'Ololuua Forest',
+  rating: 4.6,
+  price: 567,
+});
+
+testTour
+  .save()
+  .then((doc) => console.log(doc))
+  .catch((error) => console.log(error));
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port: ${port}...`);
